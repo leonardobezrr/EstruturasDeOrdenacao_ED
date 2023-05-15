47c4034ea79267f07742e2d53c06d8220f284aae
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-05-15 10:11:44 -0300

    implementando o selection sort

--- fontes/selectionSort.py
+++ fontes/selectionSort.py
@@ -1,1 +1,18 @@
-# Selection Sort tem um tempo de execução de O(n^2), ou seja, ele realiza n^2 comparações e n^2 trocas de posição de elementos.
+lista = [1,5,3,20,2,50,30]
+
+print()
+def selection_sort(lista):
+    n = len(lista)
+    for i in range(n):
+        # Encontrando o menor elemento restante na lista desordenada
+        menor = i
+        for j in range(i+1, n):
+            if lista[j] < lista[menor]:
+                menor = j
+        # Trocando o menor elemento encontrado com o primeiro elemento restante na lista desordenada
+        lista[i], lista[menor] = lista[menor], lista[i]
+    return lista
+print("Lista Desordenada: ",lista)
+print()
+selection_sort(lista)
+print("Lista Ordenada: ",lista)
