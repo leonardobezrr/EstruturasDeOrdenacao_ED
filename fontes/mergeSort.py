a5bb361faa4f0505411c470b3453bb402514fa1b
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-05-15 11:07:17 -0300

    implementando o merge sort

---
+++ fontes/mergeSort.py
@@ -0,0 +1,43 @@
+lista = [3,5,2,20,1,50,30,4]
+
+def merge_sort(lista):
+    # Condição de parada: se a lista tem apenas um elemento, ela já está ordenada
+    if len(lista) <= 1:
+        return lista
+
+    # Divide a lista ao meio
+    meio = len(lista) // 2
+    esquerda = lista[:meio]
+    direita = lista[meio:]
+
+    # Ordena recursivamente as sublistas esquerda e direita
+    esquerda = merge_sort(esquerda)
+    direita = merge_sort(direita)
+
+    # Combina as sublistas ordenadas em uma única lista ordenada
+    return merge(esquerda, direita)
+
+
+def merge(esquerda, direita):
+    listaOrdenada = []
+    i = 0  # índice para percorrer a lista esquerda
+    j = 0  # índice para percorrer a lista direita
+
+    # Combina as sublistas ordenadas
+    while i < len(esquerda) and j < len(direita):
+        if esquerda[i] < direita[j]:
+            listaOrdenada.append(esquerda[i])
+            i += 1
+        else:
+            listaOrdenada.append(direita[j])
+            j += 1
+
+    # Adiciona os elementos restantes, se houver, de uma das sublistas
+    listaOrdenada.extend(esquerda[i:])
+    listaOrdenada.extend(direita[j:])
+
+    return listaOrdenada
+
+print("Lista Desordenada: ",lista)
+print()
+print("Lista Ordenada: ", merge_sort(lista))
