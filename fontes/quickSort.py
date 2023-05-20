c8704166d25d64a4d9577cbc50a889927b62eda5
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-05-20 08:45:46 -0300

    ok

--- fontes/quickSort.py
+++ fontes/quickSort.py
@@ -1,8 +1,8 @@
 lista = [3,5,2,20,1,50,30]
-
+#O método de ordenação quick sort funciona da seguinte maneira: Um elemento da lista, é denomidado pivô, posteriormente a lista é rearranjada de forma que todos os elementos anteriores ao pivô sejam menores que ele, e todos os elementos posteriores ao pivô sejam maiores que ele. Ao fim do processo o pivô estará em sua posição final e haverá duas sub listas não ordenadas. Depois, recursivamente, a sub lista dos elementos menores são ordenadas assim como a sub lista dos elementos maiores
 def quickSort(lista):
     if len(lista) <= 1:
-        return lista #Caso a lista tenha tamanho menor ou igual a 1
+        return lista 
     else:
         pivo = lista[0] # Seleciona o pivo como o primeiro elemento da lista
         esquerda = []
