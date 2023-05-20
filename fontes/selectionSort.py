8f2ed2444d04ddf0b370e171b23e797deaecc46c
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-05-19 23:18:59 -0300

    selection sort

--- fontes/selectionSort.py
+++ fontes/selectionSort.py
@@ -2,7 +2,9 @@ import numpy as np
 from PyGnuplot import gp
 lista = [1,5,3,20,2,50,30]
 
-
+# A complexidade deste algoritmo será sempre O(n²);
+#O algoritmo selection sort não necessita de um vetor auxiliar ;
+#O selection sort compara a cada interação um elemento com os outros,  visando encontrar o menor. Dessa forma, podemos entender que não  existe um melhor caso mesmo que o vetor esteja ordenado ou em ordem  inversa serão executados os dois laços do algoritmo, o externo e o interno.
 def selectionSort(lista):
     n = len(lista)
     for i in range(n):
