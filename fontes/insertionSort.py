dc760cc92af17bead6ec12a2c32e7b82a22dae20
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-05-19 21:42:36 -0300

    insertion sort

--- fontes/insertionSort.py
+++ fontes/insertionSort.py
@@ -1,8 +1,12 @@
 lista = [1,5,3,20,2,50,30]
+# O insertion sort vai percorrer o vetor e caso o elemento anterior for menor ele troca de posição, vai repetindo esse processo até o vetor estar ordenado
+# É útil para pequenas entradas 
+# Melhor caso: O(n), quando a matriz está ordenado; 
+# Médio caso: O(n²/4), quando a matriz tem valores aleatórios sem ordem de classificação (crescente ou decrescente);
+# Pior caso: O(n²), quando a matriz está em ordem inversa, daquela que deseja ordenar.
 
 def insertionSort(lista):
     for i in range(1, len(lista)):
-        # armazena o valor do elemento atual em uma variável temporária
         eleAtual = lista[i]
         j = i - 1
         # enquanto o elemento anterior for maior que o valor atual e ainda houver elementos para iterar
@@ -14,7 +18,6 @@ def insertionSort(lista):
         # insere o valor atual na posição correta na lista
         lista[j + 1] = eleAtual
     return lista
-
 print("Lista Desordenada: ",lista)
 print()
 insertionSort(lista)
